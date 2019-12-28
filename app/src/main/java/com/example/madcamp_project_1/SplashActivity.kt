c4ba15f1fc2f3447971105b14dc6e0e54d3afc53
HyunjoonCho <jhj09016@gmail.com>
2019-12-28 15:52:28 +0900

    Splash Screen added

--- app/src/main/java/com/example/madcamp_project_1/SplashActivity.kt
+++ app/src/main/java/com/example/madcamp_project_1/SplashActivity.kt
@@ -6,11 +6,19 @@ import android.os.Bundle
 import android.os.Handler
 
 class SplashActivity: AppCompatActivity(){
+    private val SPLASH_TIME_OUT:Long=800 // ms unit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(this,MainActivity::class.java))
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
 
-        finish()
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }

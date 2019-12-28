7b53eeedea5d815dae417e7a6b7e4bc4162106d3
HyunjoonCho <jhj09016@gmail.com>
2019-12-28 21:44:23 +0900

    Copyright added

--- app/src/main/java/com/example/madcamp_project_1/SplashActivity.kt
+++ app/src/main/java/com/example/madcamp_project_1/SplashActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+//<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
 class SplashActivity: AppCompatActivity(){
     private val SPLASH_TIME_OUT:Long=800 // ms unit

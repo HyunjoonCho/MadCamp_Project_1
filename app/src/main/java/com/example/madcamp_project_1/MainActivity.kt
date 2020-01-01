67a7008b93cd2cdd17731e5f0a295e6726b7354b
KimJS0328 <wjddkel328@gamil.com>
2020-01-01 12:14:21 +0900

    Fix: gallery zoom index out of range exception

--- app/src/main/java/com/example/madcamp_project_1/MainActivity.kt
+++ app/src/main/java/com/example/madcamp_project_1/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.madcamp_project_1
 
-import android.content.Intent
 import android.os.Bundle
-import android.provider.CalendarContract
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.threetenabp.AndroidThreeTen
